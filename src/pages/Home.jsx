8a203c114493086fd1cf652a1121c5942be79de5
Lyn Resell <61920408+lynar13@users.noreply.github.com>
2025-06-23 01:01:33 +0200

    removed duplicate <main> tags

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,13 +26,13 @@ export default function Home() {
   ];
 
   return (
-    <main className='flex-grow max-w-screen-xl mx-auto px-4 py-10'>
+    <div className='flex-grow max-w-screen-xl mx-auto px-4 py-10'>
         <h1 className='text-4xl text-[#D98969] font-bold justify-center flex'>My Projects</h1>
         <section className="max-w-6xl mx-auto px-4 py-12 grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {projects.map((proj) => (
             <ProjectCard key={proj.path} {...proj} />
         ))}
         </section>
-    </main>
+    </div>
   );
 }
